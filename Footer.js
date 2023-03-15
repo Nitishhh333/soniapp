f71c767d4e60792db7c14779b27c6db498928e85
Nitish Dubey <justdubey@gmail.com>
2023-03-15 23:45:11 +0530

    SNAP-8 first commit

--- Footer.js
+++ Footer.js
@@ -3,6 +3,6 @@ export default function Footer() {
   return (
     <View style={{position: 'absolute', left: 0, right: 0, bottom: 0}}>
       <Text style={{textAlign: "center",padding: 10,backgroundColor: "#f2f2"}}>Copyright 2023</Text>
-    </View> 
+    </View>  
   );
 }
