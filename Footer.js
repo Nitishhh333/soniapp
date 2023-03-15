7c692e463db9892aebe729e7088eaec24e5d7494
Nitish Dubey <justdubey@gmail.com>
2023-03-15 23:46:29 +0530

    SNAP-8 first footer

--- Footer.js
+++ Footer.js
@@ -2,7 +2,10 @@ import {  Text, View } from "react-native";
 export default function Footer() {
   return (
     <View style={{position: 'absolute', left: 0, right: 0, bottom: 0}}>
-      <Text style={{textAlign: "center",padding: 10,backgroundColor: "#f2f2"}}>Copyright 2023</Text>
+      <Text
+      style={{textAlign: "center",padding: 10,backgroundColor: "#f2f2"}}>
+        Copyright 2023
+      </Text>
     </View>  
   );
 }
