460a56820f4a89d59c3573c881bc23f0476bcfc3
Nitish Dubey <justdubey@gmail.com>
2023-03-17 00:12:25 +0530

    SNAP-9 create login reg

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import Footer from "./src/components/Footer.js";
-export default function App() {
+import Login from "./src/components/Login.js";
+import Register from "./src/components/Register.js";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
+function DetailsScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24, textAlign: "center" }}>Main Page!!</Text>
-      <Footer />
-    </View>
+        <StatusBar style="auto" />
+        
+        
+        <TouchableOpacity style={styles.loginBtn}
+        title="Login"
+        onPress={() => navigation.navigate('Login')}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.registerBtn}
+        title="Register"
+        onPress={() => navigation.navigate('Register')}>
+          <Text>Register</Text>
+        </TouchableOpacity>
+        <Footer />
+      </View>
+    
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={DetailsScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,5 +54,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#ADD8E6",
+  },
+  registerBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#e6e600",
   },
 });
