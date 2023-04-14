478db1b3023eaa0e19e5488a31af369defd570f0
Nitish Dubey <justdubey@gmail.com>
2023-04-14 18:18:46 +0530

    SNAP-9-Landing-Page login register forgetpassword page done

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-
-import React from "react";
-import { Login, Register, LoginPage, Forgetpassword } from "./src/components/MainContent";
-import { Copyright } from "./src/components/Footer";
-
+import React from 'react';
+import { Login, Register, LoginPage, Forgetpassword } from './src/components/MainContent';
+import { Copyright } from './src/components/Footer';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import AntDesign from '@expo/vector-icons/AntDesign';
+// import AntDesign from '@expo/vector-icons/AntDesign';
 
-import Footer from './src/components/Footer.jsx';
+// import Footer from './src/components/Footer.jsx';
 // import Login from "./src/components/Login.js";
 // import Register from "./src/components/Register.js";
 
@@ -18,17 +16,13 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={LoginPage} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Forget Password" component={Forgetpassword} />
-
       </Stack.Navigator>
-      <Copyright /> 
+      <Copyright />
     </NavigationContainer>
-    
   );
 }
-
-
