92861f6483b74ddf17f478cd629696d6cc491e1b
Nitish Dubey <justdubey@gmail.com>
2023-04-15 01:00:47 +0530

    SNAP-9

--- src/components/MainContent/Register.js
+++ src/components/MainContent/Register.js
@@ -1,11 +1,10 @@
+import React, { useState } from 'react';
 import {
   Box,
-  Text,
   Heading,
   VStack,
   FormControl,
   Input,
-  Link,
   Button,
   HStack,
   Center,
@@ -13,6 +12,51 @@ import {
 } from 'native-base';
 
 function Register({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorEmail, setErroremail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+  const [errorCpassword, setErrorCPassword] = useState('');
+  // const password = useRef(null);
+  const submitHandler = () => {
+    let error = false;
+
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    if (reg.test(email) === false) {
+      setErroremail('Email is Not Correct');
+      error = true;
+    }
+    if (errorEmail.length == 0) {
+      setErroremail('This field is required');
+      error = true;
+    }
+
+    if (password.length == 0) {
+      setErrorPassword('This field is required');
+      error = true;
+    } else if (password != confirmPassword) {
+      setErrorPassword('Password Must be same');
+      setErrorCPassword('Password Must be same');
+      error = true;
+    }
+
+    if (confirmPassword.length == 0) {
+      setErrorCPassword('This field is required');
+      error = true;
+    } else if (password != confirmPassword) {
+      setErrorPassword('Password Must be same');
+      setErrorCPassword('Password Must be same');
+      error = true;
+    }
+
+    if (error == false) {
+      setErrorPassword('');
+      setErrorCPassword('');
+      setErroremail('');
+    }
+  };
+
   return (
     <NativeBaseProvider>
       <Center flex={1} px="3">
@@ -38,19 +82,22 @@ function Register({ navigation }) {
               Sign up to continue!
             </Heading>
             <VStack space={3} mt="5">
-              <FormControl>
+              <FormControl isRequired isInvalid={errorEmail.length}>
                 <FormControl.Label>Email</FormControl.Label>
-                <Input />
+                <Input value={email} type="email" onChangeText={setEmail} />
+                <FormControl.ErrorMessage>{errorEmail}</FormControl.ErrorMessage>
               </FormControl>
-              <FormControl>
+              <FormControl isRequired isInvalid={errorPassword.length}>
                 <FormControl.Label>Password</FormControl.Label>
-                <Input type="password" />
+                <Input type="password" onChangeText={setPassword} />
+                <FormControl.ErrorMessage>{errorPassword}</FormControl.ErrorMessage>
               </FormControl>
-              <FormControl>
+              <FormControl isRequired isInvalid={errorCpassword.length}>
                 <FormControl.Label>Confirm Password</FormControl.Label>
-                <Input type="password" />
+                <Input type="password" onChangeText={setConfirmPassword} />
+                <FormControl.ErrorMessage>{errorCpassword}</FormControl.ErrorMessage>
               </FormControl>
-              <Button mt="2" color="#0079ad">
+              <Button mt="2" color="#0079ad" onPress={submitHandler}>
                 Sign up
               </Button>
               <HStack mt="6" justifyContent="center">
